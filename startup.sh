4438a800fc79ff10bc4b7be2ceae461954a9bc5f
Ashley Dumaine <ashley.dumaine@gmail.com>
2017-11-06 23:40:00 -0500

    check if bluetooth and voicehat cards are detected before setting the sink and source

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-pulseaudio --start
-echo -e "connect ${BLUETOOTH_SPEAKER_ADDRESS}" | bluetoothctl && sleep 5
-pacmd set-card-profile ${BLUETOOTH_SPEAKER_CARD_NAME} a2dp
-pacmd set-card-profile alsa_card.platform-soc_sound input:analog-stereo
+pulseaudio --start --realtime --no-cpu-limit -D
+echo -e "connect ${BLUETOOTH_SPEAKER_ADDRESS}" | bluetoothctl
+if [[ `pacmd list-cards | sed -n '1 p'` == "2"* ]] ; then
+  pacmd set-card-profile ${BLUETOOTH_SPEAKER_CARD_NAME} a2dp
+  pacmd set-card-profile alsa_card.platform-soc_sound input:analog-stereo
+else
+  pacmd set-card-profile alsa_card.platform-soc_sound output:analog-stereo+input:analog-stereo
+fi
+amixer sset Master 55%
